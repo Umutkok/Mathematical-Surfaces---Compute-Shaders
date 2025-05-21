62f00ef80869594d43ea5e20e532f5b6204bc240
Umutkok <umutkok88@gmail.com>
2025-05-21 13:54:56 +0300

    First

---
+++ Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,111 @@
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary
+{
+	public delegate Vector3 Function(float u, float v, float t);
+
+	public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
+	static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
+
+	public static Function GetFunction(FunctionName name)
+	{
+		return functions[(int)name];
+	}
+
+	public static Vector3 Wave(float u, float v, float t)
+	{
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + v + t));
+		p.z = v;
+		return p;
+	}
+	public static Vector3 MultiWave(float u, float v, float t)
+	{
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + t));
+		p.y += 0.5f * Sin(2f * PI * (v + t));
+		p.y += Sin(PI * (u + v + 0.25f * t));
+		p.y *= 1f / 2.5f;
+		p.z = v;
+		return p;
+	}
+	public static Vector3 Ripple(float u, float v, float t)
+	{
+		float d = Sqrt(u * u + v * v);
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (4f * d - t));
+		p.y /= 1f + 10f * d;
+		p.z = v;
+		return p;
+	}
+	public static Vector3 Sphere(float u, float v, float t)
+	{
+		//float r = 0.5f + 0.5f * Sin(PI * t); 							//Küre
+		//float r = 0.9f + 0.1f * Sin(8f * PI * u);						//dikey çıkıntılı küre	
+		//float r = 0.9f + 0.1f * Sin(8f * PI * v);						//yatay çıkıntılı küre
+		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));        //hem dikey hem yatay twisted küre
+
+
+		float s = r * Cos(0.5f * PI * v);       //Bu s, her bir v yüksekliğine göre hesaplanan yatay çemberin yarıçapıdır. 
+												//Cos(π/2 * v) ile bu çemberlerin yarıçapı en tepede ve en altta sıfır olur, ortada maksimum olur.
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
+
+	/*/public static Vector3 Ripple (float x, float z, float t) 
+		{
+			float d = Sqrt(x * x + z * z);                // 1. d: orijinden uzaklık (radyal mesafe)
+			float y = Sin(PI * (4f * d - t));            // 2. Sinüs dalgası: zamana ve mesafeye bağlı
+			return y / (1f + 10f * d);                  // 3. Genliği mesafeye göre azalt
+		}*/
+
+
+	public static Vector3 Torus(float u, float v, float t)
+	{
+		//normal torus
+		//float r1 = 0.75f;
+		//float r2 = 0.25f;
+		//spiral torus
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+
+		float s = r1 + r2 * Cos(PI * v); //yarım daire tam daireye çevirdik dikkat!
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
+
+	public static FunctionName GetNextFunctionName(FunctionName name)
+	{
+		if ((int)name < functions.Length - 1)
+		{
+			return name + 1;
+		}
+		else
+		{
+			return 0;
+		}
+		//return (int)name < functions.Length - 1 ? name + 1 : 0; //tek satırda da yazılabilir
+	}
+
+	public static FunctionName GetRandomFunctionNameOtherThan(FunctionName name)
+	{
+		var choice = (FunctionName)Random.Range(1, functions.Length);
+		return choice == name ? 0 : choice; // aynı fonksiyonu tekrar seçmediğimize emin olduk
+	}
+
+	public static Vector3 Morph(float u, float v, float t, Function from, Function to, float progress) //fonksiyon karıştırma. Burada FunctionName yerine neden Function kullanıldı? araştır 
+	{
+		return Vector3.LerpUnclamped(from(u, v, t), to(u, v, t), SmoothStep(0f, 1f, progress)); //LerpUnclamped neden?
+	}
+
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,139 @@
+using UnityEditor;
+using UnityEngine;
+public class Graph : MonoBehaviour
+{
+    Transform[] points;
+
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+    float duration;
+    bool transitioning;
+    FunctionLibrary.FunctionName transitionFunction;
+
+
+    public enum TransitionMode { Cycle, Random }
+
+    [SerializeField]
+    TransitionMode transitionMode;
+
+
+    void Awake()
+    {
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution * resolution];
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);  // "i" ile sürekli küp üretimi sağlanıyor
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    void Update()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            	if (duration >= transitionDuration)
+            {
+				duration -= transitionDuration;
+				transitioning = false;
+			}
+        }
+        else if (duration >= functionDuration)
+        {
+            duration -= functionDuration; // 0 olmamasının sebebi fazladan geçen zamanı çöpe atmadan bir sonraki hesaplamaya taşıyarak zamanlama doğruluğunu korumak.
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        
+        if (transitioning)
+        {
+			UpdateFunctionTransition();
+		}
+		else
+        {
+			UpdateFunction();
+		}
+    }
+
+    void UpdateFunction()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
+        }
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?         //Eğer Cycle ise sıra ile değilse random fonksiyon göster
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+    
+    void UpdateFunctionTransition()
+    {
+        FunctionLibrary.Function from = FunctionLibrary.GetFunction(transitionFunction) , to = FunctionLibrary.GetFunction(function); //
+        float progress = duration / transitionDuration; //
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        {
+            if (x == resolution)
+            {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(u, v, time, from, to, progress);
+        }
+    }
+
+
+
+
+
+    //artık x ve z de update de güncelleneceği için awakede tanımlamaya gerek kalmadaı ve awakde sadeleşti ancak update ve FunctionLibrary 3 vektörü ile güncellendi 
+    //alttaki eski update
+    /*void Update()
+    {
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            //position.y = Mathf.Sin(Mathf.PI * (position.x + time)); //xküp yerine sin kullanıyoruz ki tekrarlansın
+            position.y = f(position.x, position.z, time);
+            point.localPosition = position;
+        }
+
+
+    }*/
+}
